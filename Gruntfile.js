cac4914845bd5a049ca086fc424b610de0c6d595
Vadim Hermann <hermann@billiger-mietwagen.de>
2015-09-18 10:18:45 +0200

    add media query combinator grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,54 +1,73 @@
 module.exports = function(grunt) {
 
-  // Modules
-  // grunt.loadNpmTasks('grunt-init');
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('bootcamp');
+    // Modules
+    // grunt.loadNpmTasks('grunt-init');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('bootcamp');
+    grunt.loadNpmTasks('grunt-combine-media-queries');
 
-  // Grunt Tasks
-  grunt.initConfig({
+    // Grunt Tasks
+    grunt.initConfig({
 
-    // Sass
-    sass: {
-      test: {
-        options: {
-          style: 'expanded',
+        // Sass
+        sass: {
+            test: {
+                options: {
+                    style: 'expanded',
+                },
+                files: {
+                    'test-results.css': './tests/tests.scss',
+                    './examples/styles.css': './examples/styles.scss'
+                }
+            },
+            dist: {
+                options: {
+                    style: 'compressed',
+                },
+                files: {
+                    'hill.css': 'hill.scss'
+                }
+            }
         },
-        files: {
-          'test-results.css': './tests/tests.scss',
-          './examples/styles.css': './examples/styles.scss'
-        }
-      },
-      dist: {
-        options: {
-          style: 'compressed',
+
+        // Bootcamp SCSS Unit Tests
+        bootcamp: {
+            test: {
+                files: {
+                    src: ['test-results.css']
+                }
+            }
         },
-        files: {
-          'hill.css': 'hill.scss'
+
+        // Watch
+        watch: {
+            sass: {
+                files: ['**/*.scss'],
+                tasks: ['sass', 'bootcamp', 'cmq']
+            },
+            css: {
+                files: '**/*.css',
+
+                options: {
+                    livereload: 1338
+                }
+            },
+        },
+
+        cmq: {
+            options: {
+                log: false
+            },
+            css: {
+                files: {
+                    './examples': ['examples/*.css']
+                }
+            }
         }
-      }
-    },
+    });
 
-    // Bootcamp SCSS Unit Tests
-    bootcamp: {
-      test: {
-        files: {
-          src: ['test-results.css']
-        }
-      }
-    },
-
-    // Watch
-    watch: {
-      dev: {
-        files: ['*.scss', 'libs/*.scss', 'specs/*.scss'],
-        tasks: ['sass', 'bootcamp']
-      }
-    }
-  });
-
-  // Tasks
-  grunt.registerTask('default', ['sass', 'bootcamp', 'watch']);
-  grunt.registerTask('test',    ['sass', 'bootcamp']);
+    // Tasks
+    grunt.registerTask('default', ['sass', 'cmq', 'bootcamp', 'watch']);
+    grunt.registerTask('test',    ['sass', 'bootcamp']);
 };
